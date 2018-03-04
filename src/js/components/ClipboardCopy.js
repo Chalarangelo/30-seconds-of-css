cac64a67138ea750d25d705b3ccd9652f9798f29
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-03-04 14:35:32 +0100

    run parcel and build

--- src/js/components/ClipboardCopy.js
+++ src/js/components/ClipboardCopy.js
@@ -1,34 +1,34 @@
-import { selectAll } from '../deps/utils';
+import { selectAll } from '../deps/utils'
 // import autoprefix from '../deps/'
 
 // 30-seconds-of-code snippet!
 const copyToClipboard = str => {
-    const el = document.createElement('textarea');
-    el.value = str;
-    el.setAttribute('readonly', '');
-    el.style.position = 'absolute';
-    el.style.left = '-9999px';
-    document.body.appendChild(el);
-    const selected =
-      document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-    if (selected) {
-      document.getSelection().removeAllRanges();
-      document.getSelection().addRange(selected);
-    }
-};
+  const el = document.createElement('textarea')
+  el.value = str
+  el.setAttribute('readonly', '')
+  el.style.position = 'absolute'
+  el.style.left = '-9999px'
+  document.body.appendChild(el)
+  const selected =
+    document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false
+  el.select()
+  document.execCommand('copy')
+  document.body.removeChild(el)
+  if (selected) {
+    document.getSelection().removeAllRanges()
+    document.getSelection().addRange(selected)
+  }
+}
 
-const snippets = selectAll('.snippet');
+const snippets = selectAll('.snippet')
 snippets.forEach(snippet => {
-    const css = snippet.querySelector('pre code.lang-css').textContent;
-    const autoprefixCss = css; //Hook autoprexier to this part
+  const css = snippet.querySelector('pre code.lang-css').textContent
+  const autoprefixCss = css //Hook autoprexier to this part
 
-    const copyButton = document.createElement('button');
-    copyButton.classList = 'btn is-large codepen-btn';
-    copyButton.innerHTML = '<i data-feather="copy"></i>Copy to clipboard';
-    copyButton.onclick = () => copyToClipboard(autoprefixCss);
+  const copyButton = document.createElement('button')
+  copyButton.classList = 'btn is-large codepen-btn'
+  copyButton.innerHTML = '<i data-feather="copy"></i>Copy to clipboard'
+  copyButton.onclick = () => copyToClipboard(autoprefixCss)
 
-    snippet.insertBefore(copyButton, snippet.querySelector('.snippet-demo').nextSibling); // Insert next to h4 CSS
-});
+  snippet.insertBefore(copyButton, snippet.querySelector('.snippet-demo').nextSibling) // Insert next to h4 CSS
+})
--- src/js/components/CodepenCopy.js
+++ src/js/components/CodepenCopy.js
@@ -2,27 +2,27 @@ import { selectAll } from '../deps/utils'
 
 const snippets = selectAll('.snippet')
 snippets.forEach(snippet => {
-  const codepenForm = document.createElement('form');
-  codepenForm.action = 'https://codepen.io/pen/define';
-  codepenForm.method = 'POST';
-  codepenForm.target = '_blank';
-  const codepenInput = document.createElement('input');
-  codepenInput.type = 'hidden';
-  codepenInput.name = 'data';
-  const codepenButton = document.createElement('button');
-  codepenButton.classList = 'btn is-large codepen-btn';
-  codepenButton.innerHTML = '<i data-feather="eye"></i>Edit on Codepen';
-  const css = snippet.querySelector('pre code.lang-css');
-  const html = snippet.querySelector('pre code.lang-html');
-  const js = snippet.querySelector('pre code.lang-js');
+  const codepenForm = document.createElement('form')
+  codepenForm.action = 'https://codepen.io/pen/define'
+  codepenForm.method = 'POST'
+  codepenForm.target = '_blank'
+  const codepenInput = document.createElement('input')
+  codepenInput.type = 'hidden'
+  codepenInput.name = 'data'
+  const codepenButton = document.createElement('button')
+  codepenButton.classList = 'btn is-large codepen-btn'
+  codepenButton.innerHTML = '<i data-feather="eye"></i>Edit on Codepen'
+  const css = snippet.querySelector('pre code.lang-css')
+  const html = snippet.querySelector('pre code.lang-html')
+  const js = snippet.querySelector('pre code.lang-js')
   const data = {
-    css : css.textContent,
+    css: css.textContent,
     title: snippet.querySelector('h3 > span').textContent,
     html: html ? html.textContent : '',
     js: js ? js.textContent : ''
   }
-  codepenInput.value = JSON.stringify(data);
-  codepenForm.appendChild(codepenInput);
-  codepenForm.appendChild(codepenButton);
-  snippet.insertBefore(codepenForm, snippet.querySelector('.snippet-demo').nextSibling);
-});
+  codepenInput.value = JSON.stringify(data)
+  codepenForm.appendChild(codepenInput)
+  codepenForm.appendChild(codepenButton)
+  snippet.insertBefore(codepenForm, snippet.querySelector('.snippet-demo').nextSibling)
+})
